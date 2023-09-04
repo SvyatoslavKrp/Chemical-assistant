33ba5b0fb33a734d3ceafd9071f85be78b949bd1
Sviataslau <kirpik.svyatoslav@yandex.ru>
2023-09-04 18:12:26 +0300

    smiles + keyboard

--- src/main/java/org/painting/chembot/chemistalt_bot/service/ChemistAltBot.java
+++ src/main/java/org/painting/chembot/chemistalt_bot/service/ChemistAltBot.java
@@ -1,5 +1,6 @@
 package org.painting.chembot.chemistalt_bot.service;
 
+import com.vdurmont.emoji.EmojiParser;
 import lombok.extern.slf4j.Slf4j;
 import org.painting.chembot.chemistalt_bot.config.BotConfig;
 import org.painting.chembot.chemistalt_bot.domain.User;
@@ -8,11 +9,16 @@ import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
 import org.telegram.telegrambots.meta.api.objects.Chat;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
 import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.sql.Timestamp;
@@ -26,6 +32,8 @@ public class ChemistAltBot extends TelegramLongPollingBot {
     private final BotConfig botConfig;
     private final UserRepository userRepository;
     private static final String HELP_MESSAGE = "Помощь в пути";
+    private final static String YES_BUTTON = "YES_BUTTON";
+    private final static String NO_BUTTON = "NO_BUTTON";
 
     public ChemistAltBot(BotConfig botConfig, UserRepository userRepository) {
 
@@ -76,10 +84,84 @@ public class ChemistAltBot extends TelegramLongPollingBot {
                 case "/help":
                     sendMessage(chatId, HELP_MESSAGE, username.toString());
                     break;
+                case "/register":
+                    register(chatId);
+                    break;
                 default:
                     sendMessage(chatId, "Sorry, the command is not recognized", username.toString());
             }
 
+        } else if (update.hasCallbackQuery()) {
+
+            String callbackData = update.getCallbackQuery().getData();
+            Integer messageId = update.getCallbackQuery().getMessage().getMessageId();
+            Long chatId = update.getCallbackQuery().getMessage().getChatId();
+
+            if (callbackData.equals(YES_BUTTON)) {
+
+                String text = "Вы нажали \"Да\"";
+                EditMessageText message = new EditMessageText();
+                message.setChatId(String.valueOf(chatId));
+                message.setText(text);
+                message.setMessageId(messageId);
+
+                try {
+                    execute(message);
+                } catch (TelegramApiException e) {
+                    log.error("Error occurred: " + e.getMessage());
+                }
+
+            } else if (callbackData.equals(NO_BUTTON)) {
+
+                String text = "Вы нажали \"Нет\"";
+                EditMessageText message = new EditMessageText();
+                message.setChatId(String.valueOf(chatId));
+                message.setText(text);
+                message.setMessageId(messageId);
+
+                try {
+                    execute(message);
+                } catch (TelegramApiException e) {
+                    log.error("Error occurred: " + e.getMessage());
+                }
+
+            }
+
+        }
+
+    }
+
+    private void register(Long chatId) {
+
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText("Вы точно хотите зарегестрироваться?");
+
+        InlineKeyboardMarkup markupLine = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> inlineRows = new ArrayList<>();
+        List<InlineKeyboardButton> inlineRow = new ArrayList<>();
+
+        InlineKeyboardButton yesButton = new InlineKeyboardButton();
+        yesButton.setText("yes");
+        yesButton.setCallbackData(YES_BUTTON);
+
+        InlineKeyboardButton noButton = new InlineKeyboardButton();
+        noButton.setText("no");
+        noButton.setCallbackData(NO_BUTTON);
+
+        inlineRow.add(yesButton);
+        inlineRow.add(noButton);
+
+        inlineRows.add(inlineRow);
+
+        markupLine.setKeyboard(inlineRows);
+
+        message.setReplyMarkup(markupLine);
+
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            log.error("Error occurred: " + e.getMessage());
         }
 
     }
@@ -88,9 +170,8 @@ public class ChemistAltBot extends TelegramLongPollingBot {
 
         registerUser(message);
 
-        String answer = "Hi, " + username + ", nice to meet you";
+        String answer = EmojiParser.parseToUnicode("Hi, " + username + ", nice to meet you" + ":blush:");
         sendMessage(chatId, answer, username);
-        log.info("replied to the user " + username);
 
     }
 
@@ -122,6 +203,18 @@ public class ChemistAltBot extends TelegramLongPollingBot {
         message.setChatId(String.valueOf(chatId));
         message.setText(textToSend);
         log.info("replied to the user " + username);
+
+        ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
+        List<KeyboardRow> keyboardRows = new ArrayList<>();
+        KeyboardRow row = new KeyboardRow();
+
+        row.add("SAT1");
+        row.add("SAT2");
+        row.add("Trevisan");
+
+        keyboardRows.add(row);
+        keyboardMarkup.setKeyboard(keyboardRows);
+        message.setReplyMarkup(keyboardMarkup);
 
         try {
             execute(message);
